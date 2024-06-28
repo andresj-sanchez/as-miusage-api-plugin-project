54da987352cd7a248731384678f47086b58b778e
Andres Sanchez <55501523+andresj-sanchez@users.noreply.github.com>
2024-06-28 08:13:00 -0400

    Add comments

--- plugins/as-miusage-api-plugin/src/Block/src/index.js
+++ plugins/as-miusage-api-plugin/src/Block/src/index.js
@@ -1,3 +1,12 @@
+/**
+ * @file index.js
+ * @description Entry point for the AS Miusage API Data Table block.
+ * This file registers the block with WordPress.
+ *
+ * @requires @wordpress/blocks
+ * @requires @wordpress/i18n
+ */
+
 import { registerBlockType } from '@wordpress/blocks';
 import { __ } from '@wordpress/i18n';
 
@@ -6,13 +15,19 @@ import './style.scss';
 import Edit from './edit';
 import metadata from './block.json';
 
-registerBlockType(
-	metadata.name,
-	{
-		...metadata,
-		title: __( 'AS Miusage API Data Table', 'as-miusage-api-plugin' ),
-		description: __( 'Displays data from the Miusage API', 'as-miusage-api-plugin' ),
-		edit: Edit,
-		save: () => null, // We're using ServerSideRender, so we don't need a save function
-	}
-);
+/**
+ * Register the AS Miusage API Data Table block.
+ *
+ * This block displays data from the Miusage API in a table format.
+ * It uses server-side rendering, so there's no save function defined.
+ */
+registerBlockType(metadata.name, {
+	...metadata,
+	title: __('AS Miusage API Data Table', 'as-miusage-api-plugin'),
+	description: __(
+		'Displays data from the Miusage API',
+		'as-miusage-api-plugin',
+	),
+	edit: Edit,
+	save: () => null, // We're using ServerSideRender, so we don't need a save function
+});
