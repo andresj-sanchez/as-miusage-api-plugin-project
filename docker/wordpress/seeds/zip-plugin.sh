6779cb1c6166d85cc7677b5928d5209760096bb9
Andres Sanchez <55501523+andresj-sanchez@users.noreply.github.com>
2024-06-28 12:42:19 -0400

    Fix path issue in wp dist-archive

--- docker/wordpress/seeds/zip-plugin.sh
+++ docker/wordpress/seeds/zip-plugin.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+echo $PWD
+
 # Run the dist-archive command
-wp dist-archive var/www/html/wp-content/plugins/as-miusage-api-plugin
+wp dist-archive wp-content/plugins/as-miusage-api-plugin
 
 # Move the generated zip file to a desired location (e.g., a 'dist' folder in your project root)
-mkdir -p /var/www/html/wp-content/plugins/as-miusage-api-plugin/plugin-zip
-mv as-miusage-api-plugin.*.zip /var/www/html/wp-content/plugins/as-miusage-api-plugin/plugin-zip
+mkdir -p wp-content/plugins/as-miusage-api-plugin/plugin-zip
+mv wp-content/plugins/as-miusage-api-plugin.*.zip wp-content/plugins/as-miusage-api-plugin/plugin-zip
 
 echo "Plugin zipped successfully. Check the 'dist' folder."
